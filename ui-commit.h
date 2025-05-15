3dd92de14e9f4a714e1146c7113d945b93d90461
RunasSudo <runassudo@yingtongli.me>
2025-05-15 17:38:13 +1000

    Add (void) to relevant function declarations

--- ui-commit.h
+++ ui-commit.h
@@ -1,6 +1,6 @@
 #ifndef UI_COMMIT_H
 #define UI_COMMIT_H
 
-extern void cgit_print_commit();
+extern void cgit_print_commit(void);
 
 #endif /* UI_COMMIT_H */
--- ui-tree.h
+++ ui-tree.h
@@ -4,6 +4,6 @@
 extern void cgit_tree_print_binary_buffer(char *buf, unsigned long size);
 extern void cgit_tree_print_text_buffer(const char *name, char *buf, unsigned long size);
 
-extern void cgit_print_tree();
+extern void cgit_print_tree(void);
 
 #endif /* UI_TREE_H */
