cca01672a2905d674b6f8aba2b8cd415fb10dc94
RunasSudo <runassudo@yingtongli.me>
2025-05-16 20:10:57 +1000

    Sort repos by age on index page

--- ui-repolist.h
+++ ui-repolist.h
@@ -3,6 +3,7 @@
 
 extern int cgit_repolist_is_visible(struct cgit_repo *repo);
 extern void cgit_repolist_print_modtime(struct cgit_repo *repo);
+extern int cgit_repolist_sort_repolist(char *field);
 
 extern void cgit_print_repolist(void);
 extern void cgit_print_site_readme(void);
